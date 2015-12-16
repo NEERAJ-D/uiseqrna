154a5dd4e68c7be05d581af4bb6e0f36f321eb8d
NEERAJ-D <neeraj.e.devadath@gmail.com>
2015-12-16 05:39:25 -0800

    Statistics request
    Statistics request

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ var connection = mysql.createConnection({
   database: 'rnaseq'
 });
 connection.connect();
+
+
+app.get('/rnaseq/home/stats',function(req,res){
+
+var tablename;
+
+for(var propName in req.query)
+{
+if(req.query.hasOwnProperty(propName))
+{
+console.log(propName,req.query[propName]);
+}
+if(propName == 'type')
+{tablename = req.query[propName];}
+}
+
+var sqlstmt = 'select * from '+tablename;
+var queryexec = connection.query(sqlstmt,  function(err,rows,fields) {
+console.log(queryexec.sql);
+//console.log(rows);
+res.writeHead(200,{"Content-Type":"text/json","Access-Control-Allow-Origin":"*"});
+res.end(JSON.stringify(rows));
+});
+});
